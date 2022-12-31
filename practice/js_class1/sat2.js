7b4980e440e9832b785365d8a91f3cf162b3f4c9
soumya3969 <srmhnt2001@gmail.com>
2022-12-31 17:58:11 +0530

    js practice

---
+++ practice/js_class1/sat2.js
@@ -0,0 +1,60 @@
+// 7 //int
+// 2.5 //float
+// //in js both are number
+
+// ("Soumya")
+// ('soumya')
+// //sting values
+
+// true =>1
+// false =>0
+// //Boolean values
+
+// null
+// undefined
+// //empty Values
+
+// //primitive datatype => only single value
+
+// // non primitive =>multiple value
+// //array And Object
+// [keep all string values inside this]
+// [1,2,3,4,5,6,7]
+// [a,1,2,'soumya',true,b]
+//  [1 ,2 ,3 ,4 ,5 ,6 ]; //--> array
+// //0  1  2  3  4  5   ->position  => index
+
+// //{}  => object
+
+// {
+//     key:value
+//     //key can be anything
+//     srm:soumya
+//     one:value3
+//     fname:soumya
+//     lname:mahanta
+
+// }
+
+
+//variables => place-holder for values.
+
+//var y = 25;
+// let x = 25;
+// const z = '33679270200'; //constant(value doesnt change in period of time) like bank account number.
+// let bal = 15000; //bank balance keep changing so cant be constant 
+
+
+let firstname = "Soumya";
+let lastname = "mahanta";
+const Mobmo = 9937446657;
+
+// console.log("My first name is: ",firstname);
+// console.log("MY last name is:" + lastname);
+// console.log("my number is",Mobmo,"My first name is: ",firstname);
+
+
+//template literal
+
+console.log(`my first name is ${firstname},my lastname is ${lastname},my contactno is ${Mobmo}`);
+
