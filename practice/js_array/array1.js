edeac6de6dccf2bc87c2a0f5be8be757e1d8caba
soumya3969 <srmhnt2001@gmail.com>
2023-01-09 22:04:12 +0530

    gaming site html completed

---
+++ practice/js_array/array1.js
@@ -0,0 +1,59 @@
+//array in depth
+//array method
+let names = ['hitesh','soumya','anurag','lipu','somnath'];
+// console.log(names);
+// console.log(names.length);
+// console.log(names[3],names[4]);
+// console.log(names[names.length - 1]);
+// names[4] = 'sameer';
+// console.log(names);
+
+
+//alternative way:-
+let namess = new Array('name-1','name-2'); //not recomended;
+// console.log(namess);
+
+//to insert into array
+//push => insert new valuses inside the Array.
+// names.push('litu');
+// console.log(names);
+
+//slice=>
+
+// console.log(names.slice(1,5));
+
+//splice=> to insert value inside array.
+
+let fruit = ["Apple",'Bada apple','chhota apple',"double apple"];
+// fruit.splice(2,0,'kharab apple','achha apple');
+//fruit.splice(2,1,'kharab apple','achha apple');
+//console.log(fruit);
+
+// concatenation
+
+let arr1 = [1,2,3,4];
+let arr2 = [4,5,6,7];
+let arr3 = [6,7,6,7];
+
+//console.log(arr1.concat(arr2,arr3));
+
+// fill=> HW
+
+let arr4 =[1,2,3,4,5,6,7,8,9];
+// arr4.fill('soumya',2,4);
+// console.log(arr4);
+
+// include
+
+let num=[1,2,3,4,5,6,7,8,9];
+// console.log(num.includes(7,7));
+
+
+//indexof
+
+let nums=[1,2,3,'sameer',5,6,7,8,'soumya'];
+// console.log(nums.indexOf('sameer'));
+
+console.log(Math.random()); //0 - 0.999999999999999999999 (0-1)
+console.log(Math.random() * 10);
+console.log(Math.round(Math.random() * 10));
---
+++ practice/js_class3/object.js
@@ -0,0 +1,44 @@
+//Object:-
+
+//console.log(this);
+
+const object ={
+    rocket: '🚀',
+    home: '🏡'
+}
+for(k in object){
+    console.log("value is :",k);
+}
+
+// string,number,bigint,boolean,null,
+
+
+const obj1 = {
+    rocket: '🚀',
+    fuel : 2,
+    config: {
+        name:'mars'
+    }
+}
+console.log(obj1);
+obj1.fuel = 200
+obj1['year'] = 2000
+
+
+// p2
+const obj2 = new Object() //constructor
+
+
+
+
+// p3
+const powers ={
+
+}
+
+
+
+
+
+// MDN practice object from MDN:
+
