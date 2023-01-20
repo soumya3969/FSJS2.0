6a09770e9764aabfcd0de367ebc920268cb04ffc
soumya3969 <srmhnt2001@gmail.com>
2023-01-20 19:22:12 +0530

    function-01

---
+++ practice/js_stage1/function.js
@@ -0,0 +1,14 @@
+// function:=>
+
+function sum() {
+    let num1 = 16;
+    let num2 = 25;
+
+
+    console.log('addition of two number are: ', num1 + num2);
+}
+function summ() {
+    let n1=10;
+    let n2=15;
+    console.log('the sum is: ',n1+n2);
+}
