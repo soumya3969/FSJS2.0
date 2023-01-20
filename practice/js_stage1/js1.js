97b78cfcffce93c9980b866d2b1ed6e5f2418668
soumya3969 <srmhnt2001@gmail.com>
2023-01-20 19:13:45 +0530

    js stage 1

---
+++ practice/js_stage1/js1.js
@@ -0,0 +1,18 @@
+// java script stage one:-
+
+//function and Object:-
+//datatypes:- numbers, stringd boolean, null.undrfined
+//array and objects
+// operator
+
+
+// conditions:-
+//if, if else, switch, and ternary conditions
+// loops
+// do while, while,for
+//math and date
+//array and methods=> assignment to write articles
+//for of .. =>array and for in=>object
+
+
+//Function
