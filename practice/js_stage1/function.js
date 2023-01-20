770b15195d3b5fc355101bc367ffb4f28776d815
soumya3969 <srmhnt2001@gmail.com>
2023-01-20 20:14:30 +0530

    function-01

--- practice/js_stage1/function.js
+++ practice/js_stage1/function.js
@@ -1,14 +1,28 @@
-// function:=>
 
-function sum() {
-    let num1 = 16;
-    let num2 = 25;
-
-
-    console.log('addition of two number are: ', num1 + num2);
-}
+// value1& value2 =parameter
 function summ() {
-    let n1=10;
-    let n2=15;
-    console.log('the sum is: ',n1+n2);
+    let n1 = 10;
+    let n2 = 15;
+    console.log('the summ is: ',n1 + n2);
 }
+summ()
+function sum(value1,value2) {
+    // let n1 = 10;
+    // let n2 = 15;
+    console.log('the summ is: ',value1 + value2);
+}
+
+sum(10,30);
+sum(25, 35);
+sum(1, 5);
+
+let a = 'soumya';
+let b = 35;
+console.log(a + b);
+
+// normal function  & function with argument 
+
+// function with return file
+
+ 
+
