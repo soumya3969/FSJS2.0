3d3954745f2c4fb750fbc0d368bdd6798db53078
soumya3969 <srmhnt2001@gmail.com>
2023-01-21 14:20:44 +0530

    arrow-in-js

---
+++ practice/js_stage1/arrow.js
@@ -0,0 +1,18 @@
+// => Arrow (it will become function automatically)
+
+let jaimin = () => {
+    console.log(' im god');
+};
+jaimin();
+
+let shiva =(v1,v2) => {
+    console.log("im GOD ",v1,v2);
+}
+shiva(1,2);
+
+let manas =()=>{
+    console.log("manas");
+}
+
+manas();
+
--- practice/js_stage1/string.js
+++ practice/js_stage1/string.js
@@ -19,3 +19,22 @@ function Number(arr) {
 let Numberarray = [1,2,3,4,5,6,7,8,9];
 let arrresult = Number(Numberarray);
 console.log(arrresult);
+
+
+function sr(){
+    // arguments
+    // console.log(arguments);
+    let sum2 = 0;
+    for(let i = 0; i < arguments.length;i++){
+        sum2 = sum2 + arguments[i];
+    }
+    return sum2;
+}
+
+// sr(7, 8, 9, 10, 11, 12, 13, 14, 15);
+console.log(sr(7, 8, 9, 10, 11, 12, 13, 14, 15));
+
+
+
+
+// => Arrow (it will become function automatically)
