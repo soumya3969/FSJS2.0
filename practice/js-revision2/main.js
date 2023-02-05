b9932265f6e2f39606e1f7892432e0eb51ab37ae
soumya3969 <srmhnt2001@gmail.com>
2023-02-05 15:50:20 +0530

    class 5th feb

---
+++ practice/js-revision2/main.js
@@ -0,0 +1,17 @@
+var a =10;
+console.log(a);
+
+function sum(){
+    var a =10;
+    var b =20;
+    console.log(a - b);
+
+}
+sum();
+
+function sub(){
+    var a =10;
+    var b =20;
+    console.log(a - b);
+}
+sub();
