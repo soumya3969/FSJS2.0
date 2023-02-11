d174b66e990a42267c6fe849e1d4421a99c03107
soumya3969 <srmhnt2001@gmail.com>
2023-02-11 19:57:38 +0530

    weather app update

--- Weather App/main.js
+++ Weather App/main.js
@@ -6,7 +6,7 @@
 let data;
 
 const inputBox = document.getElementById("inputBox");
-const countryname = document.getElementById("countryName");
+const countryName = document.getElementById("countryName");
 const stateName = document.getElementById("stateName");
 const cityName = document.getElementById("cityName");
 const humidity = document.getElementById("humidity");
@@ -28,18 +28,22 @@ const getData = async (event) => {
     // Fetch details
     const fetchData = await fetch(`https://api.weatherapi.com/v1/current.json?key=2330b4cff9df477bb0a105928231102&q=${city}`);
 
-    const orgData = await fetchDatabase.json();
+    const orgData = await fetchData.json();
     data = orgData;
     console.log(data);
+
+    // display the
+    countryName.innerHTML = data.location.country;
+    stateName.innerHTML = data.location.region;
+    cityName.innerHTML = data.location.name;
+    humidity.innerHTML = data.current.humidity;
+    windSpeed.innerHTML =data.current.wind_kph;
+    logoImage.src = data.current.condition.icon;
+    weatherStatus.innerHTML = data.current.condition.text;
+    temperature.innerHTML = data.current.temp_c;
 };
 
-// display the
-countryname.innerHTML = data.location.country;
-stateName.innerHTML = data.location.region;
-cityName.innerHTML = data.location.name;
 
-logoImage.src = data.current.condition.icon;
-weatherStatus.innerHTML = data.current.condition.text;
 
 
 
