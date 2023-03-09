6d0189ee973708bd0bfc3a88a859b4e8ea167425
soumya3969 <srmhnt2001@gmail.com>
2023-03-09 20:45:48 +0530

    js assignment

--- JS Assignment/Js-Assignments/Q11-index.js
+++ JS Assignment/Js-Assignments/Q11-index.js
@@ -6,3 +6,12 @@
 //     - What is the hours now?
 //     - What is the minutes now?
 //     - Find out the numbers of seconds elapsed from January 1, 1970 to now
+
+const so = new Date();
+console.log(so.getFullYear()); //2023
+console.log(so.getMonth()); //2 (march)
+console.log(so.getDay()); //4
+console.log(so.getHours()); //20
+console.log(so.getMinutes()); //44
+console.log(so.getDate()); //9
+console.log(Math.round(so.getTime()/1000)); //1678374847
