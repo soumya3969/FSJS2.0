69a9d2bd88a5b646fa54df4387b16b7b0af8e416
soumya3969 <srmhnt2001@gmail.com>
2023-04-10 01:45:01 +0530

    live-code-session

---
+++ practice/08-04-2023/src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import asyncHandler from "../service/asyncHandler";
+
+// sign a new user in the database
+export const signUp = asyncHandler(async(req,res)=>{
+    // get the data from the user
+    const {name,email,password} = req.body;
+
+    // validations
+
+    if(!name ||!email || !password){
+        throw new CustomError("Please enter all the required fields",400)
+    }
+
+
+    User
+})
---
+++ practice/08-04-2023/src/service/asyncHandler.js
@@ -0,0 +1,26 @@
+
+const asyncHandler = (fn) => async(req,res,next) =>{
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.code ||500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+export default asyncHandler;
+
+// req=>from frontend
+// res=>
+
+
+
+
+
+
+// const asyncHandler = "Hitesh"
+// const asyncHandler = ( )=>{}
+// const asyncHandler = ( func )=>{}
+// const asyncHandler = ( func )=>()=>{}
+// const asyncHandler = ( func )=> async()=>{}
