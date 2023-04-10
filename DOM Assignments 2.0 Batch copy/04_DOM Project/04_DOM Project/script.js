8561058e7235731af959073c31f6af0582388f05
soumya3969 <srmhnt2001@gmail.com>
2023-04-10 19:49:52 +0530

    dom assignment 4 completed

---
+++ DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
@@ -0,0 +1,18 @@
+const barb = document.querySelector(".barb");
+barb.style.backgroundColor ="#ec9b3b";
+const archer = document.querySelector(".arch");
+archer.style.backgroundColor="#EF6A97";
+const gian = document.querySelector(".gian");
+gian.style.backgroundColor = "#E39D4C";
+const gobl = document.querySelector(".gobl");
+gobl.style.backgroundColor = "#8CC33E";
+const wiz = document.querySelector(".wiz");
+wiz.style.backgroundColor = "#4EACFF";
+
+// const red =  document.querySelectorAll(".stat");
+// red.style.color="red";
+const white = document.querySelectorAll(".one-third");
+// white.style.color="white";
+white.forEach((col)=>{
+    col.style.color="white";
+});
