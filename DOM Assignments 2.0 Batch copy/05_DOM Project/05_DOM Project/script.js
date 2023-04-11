cabc142e02302dd25d5246479c9f531a3ca93390
soumya3969 <srmhnt2001@gmail.com>
2023-04-11 19:18:24 +0530

    dom assignment 5 completed

---
+++ DOM Assignments 2.0 Batch copy/05_DOM Project/05_DOM Project/script.js
@@ -0,0 +1,13 @@
+// Create a new Nav Button as Pro Subscription
+// const newButton = document.createElement('a');
+// newButton.classList.add('btn');
+// newButton.href ="#";
+// newButton.innerText = "Pro Subscription";
+
+
+
+
+const newNavBtn = document.createElement('a');
+newNavBtn.classList.add('btn');
+newNavBtn.href = '#';
+newNavBtn.innerText = 'Pro Subscription';
