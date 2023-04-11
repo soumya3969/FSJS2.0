4993706555ceb2073f503d051b2cfe84e42d0337
soumya3969 <srmhnt2001@gmail.com>
2023-04-11 20:10:06 +0530

    dom assignment 5 completed

--- DOM Assignments 2.0 Batch copy/05_DOM Project/05_DOM Project/script.js
+++ DOM Assignments 2.0 Batch copy/05_DOM Project/05_DOM Project/script.js
@@ -1,13 +1,25 @@
-// Create a new Nav Button as Pro Subscription
-// const newButton = document.createElement('a');
-// newButton.classList.add('btn');
-// newButton.href ="#";
-// newButton.innerText = "Pro Subscription";
-
-
-
-
+// task - 1
 const newNavBtn = document.createElement('a');
 newNavBtn.classList.add('btn');
 newNavBtn.href = '#';
 newNavBtn.innerText = 'Pro Subscription';
+
+
+const navBtn = document.querySelector('#navBtn');
+navBtn.appendChild(newNavBtn);
+
+
+// task - 2
+const tagContainer = document.querySelector('.tags-container div');
+const newTag = document.createElement('a');
+newTag.href = '#';
+newTag.innerText = 'Chinese(7)';
+
+tagContainer.appendChild(newTag);
+
+// task - 3
+const recipeName = document.querySelectorAll('.recipe-name');
+recipeName.forEach((val)=>{val.style.color = '#5555f4'});
+
+const recipeDisp = document.querySelectorAll('.recipe-disp');
+recipeDisp.forEach(val => val.style.color = '#4a4ae8');
