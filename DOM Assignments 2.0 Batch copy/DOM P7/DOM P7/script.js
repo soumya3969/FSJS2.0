985e29a0109a985e836740b8f102f02ce96bef32
soumya3969 <srmhnt2001@gmail.com>
2023-04-11 20:35:56 +0530

    dom assignment 7 completed

---
+++ DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/script.js
@@ -0,0 +1,29 @@
+const all_lang_name = document.querySelectorAll(".main__languages a");
+
+const all_lang_arr = [...all_lang_name];
+
+all_lang_arr.forEach((ele) => {
+   return ele.textContent.includes("2.0")? ele.style.display = "none":``;
+});
+
+
+
+const input = document.querySelector(".main__form-input");
+input.removeAttribute("disabled");
+input.setAttribute("placeholder","iNeuron");
+
+const submit = document.querySelector(".main__form-btn");
+submit.removeAttribute("disabled");
+
+
+const form = document.querySelector("form");
+form.addEventListener("submit",(e)=>{
+   for(let ele of all_lang_arr){
+      if (ele.textContent.includes("2.0")) {
+         ele.style.display = "inline";
+      }
+   }
+   input.value  = "";
+   e.preventDefault();
+
+});
