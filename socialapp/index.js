238e86e27a797ab8fedebd7765db018329a82332
soumya3969 <srmhnt2001@gmail.com>
2023-04-16 19:44:06 +0530

    social app/express class start 1 completed

--- socialapp/index.js
+++ socialapp/index.js
@@ -20,6 +20,32 @@ app.get("/", (req, res) =>{
     res.send("<center><h1>Hello Fsjs 2.0</h1></center>"); //!res.send used to display on web page
 });
 
+app.get("/insta",(req,res) =>{
+    // res.status(200).send("Hello Fsjs web developer!"); 
+    const insta ={
+        userName: "soumya",
+        follow: 1,
+        followers: 20,
+    };
+    res.status(200).json({insta}); //! we can use json instead of send
+});
+app.get("/linked",(req,res) =>{
+    const linked = {
+        userName: "soumya",
+        connects: 159,
+        followers: 20,
+    };
+    res.status(200).json({linked}); 
+});
+app.get("/twitter",(req,res) =>{
+    const twitter ={
+        userName: "soumya",
+        followers: 20,
+        following:15
+    }
+    res.status(200).json({twitter}); 
+});
+
 // *LISTEN
 app.listen(PORT,() =>{
     console.log("Server Running");
