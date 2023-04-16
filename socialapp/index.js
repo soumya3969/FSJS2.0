95b9cdc5342eb4991a55ae9875b478166bc9535a
soumya3969 <srmhnt2001@gmail.com>
2023-04-16 16:36:35 +0530

    index.js update

--- socialapp/index.js
+++ socialapp/index.js
@@ -1,1 +1,23 @@
-console.log("hello Web Dev");
+// *1.Bring Express
+// *2.Assign Express
+// *3.PORT(Excuted)
+// *4.Listen
+// *5.Route
+    // ?Middleware
+    // ?models
+    // ?database
+
+// *Bring Express
+const express = require('express');
+// *Assign Express
+const app = express();
+// *PORT
+const PORT = 3000;
+        // !Procee.env.Port
+// *LISTEN
+app.listen(PORT,() =>{
+    console.log("Server listening on "+PORT);
+})
+
+
+
