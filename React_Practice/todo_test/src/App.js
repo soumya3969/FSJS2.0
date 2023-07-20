7f4bd38bf743b72c3736d7f77a5d7300035ef07d
soumya <117825720+soumya3969@users.noreply.github.com>
2023-07-20 06:28:47 +0000

    rrrr

--- React_Practice/todo_test/src/App.js
+++ React_Practice/todo_test/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import Logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./logo.svg";
+import "./App.css";
 
+// function App() {
+//   return(
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={Logo} alt="logo" className="App-logo" />
+//       </header>
+//     </div>
+//   );
+// }
 
-
-function App() {
-  return(
-    <div className="App">
-      <header className="App-header">
-        <img src={Logo} className="App-logo" />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={Logo} alt="logo" className="logo" width="100" height="100" />
+        <h1 className="app-title">Lco Todo app</h1>
+        <div className="container">
+          Add an Item .....
+          <br/>
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Enter a Item ...."
+          />
+          <
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
