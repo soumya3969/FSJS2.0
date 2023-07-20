62cd2de575695e54dcc592dbc1897da0a814812b
soumya <117825720+soumya3969@users.noreply.github.com>
2023-07-20 08:55:14 +0000

    list css

--- React_Practice/todo_test/src/App.js
+++ React_Practice/todo_test/src/App.js
@@ -15,18 +15,31 @@ import "./App.css";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="main">
         <img src={Logo} alt="logo" className="logo" width="100" height="100" />
-        <h1 className="app-title">Lco Todo app</h1>
+        <h1 className="app-title">LCO Todo App</h1>
         <div className="container">
           Add an Item .....
-          <br/>
+          <br />
           <input
             type="text"
             className="input-text"
             placeholder="Enter a Item ...."
           />
-          <
+          <button className="add-btn">Add Todo</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" id="" checked />
+                List ! &nbsp;&nbsp;&nbsp;&nbsp;
+                <button className="btn">Delete</button>
+                {/* <div class="checkbox-wrapper-11">
+                  <input id="02-11" type="checkbox" name="r" value="2" />
+                  <label for="02-11">To-do</label>
+                </div> */}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
